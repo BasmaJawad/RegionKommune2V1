63cf66cc8b67913bfea77675effd52bf261087e3
ERLMKea <erlm@kea.dk>
2023-03-10 10:17:17 +0100

    findByKommuneNavn

--- src/main/java/com/example/regionkommune2v1/controller/KommuneRESTController.java
+++ src/main/java/com/example/regionkommune2v1/controller/KommuneRESTController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +23,20 @@ public class KommuneRESTController {
     public List<Kommune> getAllRegions() {
         return kommuneRepository.findAll();
     }
+
+    @GetMapping("kommune/kode/{code}")
+    public Kommune getKommuneByKode(@PathVariable String code) {
+        return kommuneRepository.findById(code).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Kommune med kode="+code));
+    }
+
+    @GetMapping("kommune/navn/{name}")
+    public Kommune getKommuneByName(@PathVariable String name) {
+        System.out.println("navn=" + name);
+        Optional<Kommune> kom = kommuneRepository.findKommuneByNavn(name);
+        System.out.printf("kom=" + kom.isPresent());
+        return kommuneRepository.findKommuneByNavn(name).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Kommune med navn="+name));
+    }
+
 
     @PostMapping("/kommune")
     @ResponseStatus(HttpStatus.CREATED)
--- src/main/java/com/example/regionkommune2v1/repository/KommuneRepository.java
+++ src/main/java/com/example/regionkommune2v1/repository/KommuneRepository.java
@@ -3,5 +3,14 @@ package com.example.regionkommune2v1.repository;
 import com.example.regionkommune2v1.model.Kommune;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface KommuneRepository extends JpaRepository<Kommune, String> {
+
+    Optional<Kommune> findByNavn(String navn);
+    Optional<Kommune> findKommuneByNavn(String navn);
+    List<Kommune> findKommuneByRegionKode(String kode);
+
+
 }
